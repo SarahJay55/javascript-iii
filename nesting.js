2bc04e76af839b98201ae9121142b6551c67a2c7
SarahJay55 <30481405+SarahJay55@users.noreply.github.com>
2017-09-06 17:55:18 -0700

    Nesting Exercise

--- nesting.js
+++ nesting.js
@@ -38,7 +38,16 @@ var employees = [{
 
 // Code here
 
-
+function employeeUpdater () {
+  for (var i = 0; i < employees.length; i++) {
+    if (employees[i]["firstName"] === "Theo") {
+      employees.splice(i, 1);
+    } else if (employees[i]["firstName"] === "Lorie") {
+      employees[i].department = "HR";
+    }
+  }
+  return employees;
+}
 
 
 
@@ -55,6 +64,23 @@ var employees = [{
 var workplaceAccidents = [12, 56, 44, 3, 29, 56, 56, 3, 7, 12];
 
 // Code here
+
+function removeDuplicates () {
+  var newArr = [];
+  var flag;
+  for (var i = 0; i < workplaceAccidents.length; i++) {
+    flag = true;
+    for (var j = 0; j < newArr.length; j++) {
+      if (workplaceAccidents[i] === newArr[j]) {
+        flag = false;
+      }
+    }
+    if (flag){
+      newArr.push(workplaceAccidents[i])
+    }
+  } 
+  return newArr;
+}
 
 
 
